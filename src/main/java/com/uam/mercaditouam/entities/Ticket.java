2ea8b2ee44f45aaf5fad0e904ea28137157f3528
Draxton27 <danielito271204@gmail.com>
2024-06-03 14:28:56 -0600

    JSON parse error

--- src/main/java/com/uam/mercaditouam/entities/Ticket.java
+++ src/main/java/com/uam/mercaditouam/entities/Ticket.java
@@ -1,6 +1,7 @@
 package com.uam.mercaditouam.entities;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -19,6 +20,7 @@ public class Ticket {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "CIF_Solicitante", referencedColumnName = "CIF")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private Student requesterStudent;
 
     @Column(name = "Descripcion_Problema")
